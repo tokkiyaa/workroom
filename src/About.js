8122bcededdf9a8811d5c29e2cb610f4a7b761c7
tokkiyaa <tokkiyaa@users.noreply.github.com>
2019-09-14 15:41:07 +0900

    aboutにworkroomの概要を少しだけ記載(とりあえず腹は膨れたので)

--- src/About.js
+++ src/About.js
@@ -1,40 +1,47 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import Header from './Header';
-import Grid from '@material-ui/core/Grid';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Header from "./Header";
+import Grid from "@material-ui/core/Grid";
 
 const styles = theme => ({
-  root: {
-    flexGrow: 1,
-    padding: theme.spacing(1),
-    paddingTop: theme.spacing(10),
-  },
-  caption: {
-    textAlign: "center",
-    width: "100%",
-  },
+    root: {
+        flexGrow: 1,
+        padding: theme.spacing(1),
+        paddingTop: theme.spacing(10)
+    },
+    caption: {
+        textAlign: "center",
+        width: "100%"
+    }
 });
 
 const About = props => {
-  const { classes, user } = props;
-  return (
-    <React.Fragment>
-      <Header user={user} login="/Login/target/about" />
-      <Grid container justify="center" alignItems="center" direction="row" className={classes.root}>
-          <Grid className={classes.caption}>
-          <Typography component="h2" variant="h5" gutterBottom>
-            Stay Hungry, Stay Foolish. 
-          </Typography>
-          </Grid>
-      </Grid>
-    </React.Fragment>
-  );
-}
+    const { classes, user } = props;
+    return (
+        <React.Fragment>
+            <Header user={user} login="/Login/target/about" />
+            <Grid
+                container
+                justify="center"
+                alignItems="center"
+                direction="row"
+                className={classes.root}
+            >
+                <Grid className={classes.caption}>
+                    <Typography component="h2" variant="h5" gutterBottom>
+                        WorkRoom is a tool for gathering same hard work time
+                        with some persons.
+                    </Typography>
+                </Grid>
+            </Grid>
+        </React.Fragment>
+    );
+};
 
 About.propTypes = {
-  classes: PropTypes.object.isRequired,
+    classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(About);
