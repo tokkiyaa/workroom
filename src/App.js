83d409c10345d9a7a7fe3236b1a9ac71b8264d2d
tokkiyaa <tokkiyaa@users.noreply.github.com>
2019-09-14 18:49:11 +0900

    とりあえずルーム一覧が出るようにはした。チャットルームの実装までは終わらずにタイムアップ。お疲れ様でした

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import "firebase/firestore";
 import config from "./config";
 
 firebase.initializeApp(config);
-const db = firebase.firestore();
+export const db = firebase.firestore();
 
 function App() {
     const [user, setUser] = useState(null);
